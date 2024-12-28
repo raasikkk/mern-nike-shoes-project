f16537e5abe6ebaee080dadd36b9478a6e512c1f
Yermekov Aibar <160598362+F1awless77@users.noreply.github.com>
2024-12-28 22:12:26 +0600

    feat: Added connection to MongoDB cloud database

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,24 @@
-import express from 'express'
+import express from "express";
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
-const app = express()
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected to MongoDB")
+  } catch(error) {
+    console.log(error)
+  }
+}
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  connect();
+  console.log(`Server is running on port ${PORT}`);
+});
