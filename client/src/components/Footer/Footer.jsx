3f5f1feed08e5cc6bd36613f777fef6f6280a052
Yermekov Aibar <160598362+F1awless77@users.noreply.github.com>
2024-12-31 01:03:30 +0600

    feat: Somewhat did the Footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,40 +1,47 @@
-import styles from "./footer.module.css";
+import "./footer.css";
 import { Link } from "react-router";
 
 const Footer = () => {
   return (
     <footer>
-      <div className={styles.footer}>
-        <div className={styles["top-footer"]}>
+      <div className="">
+        <div className="top-footer">
+          <div className="interactive-top-footer flex">
+            <div className="column-footer flex flex-col">
+              <Link to="/" className="title-column-footer">
+                About
+              </Link>
+              <Link to="/">About Us</Link>
+              <Link to="/">Features</Link>
+              <Link to="/">News & Blog</Link>
+            </div>
 
-          <div className={styles["logo-footer"]}>
+            <div className="column-footer flex flex-col">
+              <Link to="/" className="title-column-footer">
+                Connect
+              </Link>
+              <Link to="/">LinkedIn</Link>
+              <Link to="/">Facebook</Link>
+              <Link to="/">Twitter</Link>
+              <Link to="/">Instagram</Link>
+            </div>
 
+            <div className="column-footer flex flex-col">
+              <Link to="/" className="title-column-footer">
+                Support
+              </Link>
+              <Link to="/">FAQs</Link>
+              <Link to="/">Support Center</Link>
+              <Link to="/">Contact Us</Link>
+            </div>
           </div>
 
-          <div className={styles["column-footer"]}>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
+          <div className="logo-container">
+            <img className="logo" src="/nike-logo-001.svg" alt="Logo"></img>
           </div>
-
-          <div className={styles["column-footer"]}>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-          </div>
-
-          <div className={styles["column-footer"]}>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-            <Link to="/"></Link>
-          </div>
-
         </div>
 
-        <div className={styles["bot-footer"]}>
+        <div className="bot-footer flex items-center justify-center">
           <p>© 2025 | Nike | All Rights Preserved | Just Do It </p>
         </div>
       </div>
