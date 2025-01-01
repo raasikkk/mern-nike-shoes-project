10f83f115dd8c410fed49ca1badc1ea048759dad
Yermekov Aibar <160598362+F1awless77@users.noreply.github.com>
2025-01-01 17:48:34 +0600

    feat: Improved responsiveness. Added menu button for smaller viewports

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,56 +1,92 @@
-import styles from "./Header.module.css";
+import "./Header.css";
+import { useState } from "react";
 import { Link } from "react-router";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
-      <div className={styles.header}>
-        <div className={styles["logo-wrapper"]}>
+      <div className="header">
+        <div className="logo-wrapper">
           <Link to="/">
-            <img
-              className={styles.logo}
-              src="/nike-logo-001.svg"
-              alt="Logo"
-            ></img>
+            <img className="logo" src="/nike-logo-001.svg" alt="Logo"></img>
           </Link>
         </div>
 
-        <div className={styles["navbar-wrapper"]}>
-          <ul>
-            <li>
-              <Link to="/catalog">Products</Link>
-            </li>
-            <li>
-              <Link to="/">Pricing</Link>
-            </li>
-            <li>
-              <Link to="/">About</Link>
-            </li>
-            <li>
-              <Link to="/">FAQs</Link>
-            </li>
-          </ul>
+        {/* Bigger Screen Navbar */}
+        <div className="navbar-wrapper">
+          <nav className="navbar">
+            <ul>
+              <li>
+                <Link to="/catalog">Products</Link>
+              </li>
+              <li>
+                <Link to="/">Pricing</Link>
+              </li>
+              <li>
+                <Link to="/">About</Link>
+              </li>
+              <li>
+                <Link to="/">FAQs</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
 
-        <div className={styles["action-wrapper"]}>
-          <button className={styles.action}>
+        <div className="action-wrapper">
+          <button className="action">
             <Link to="/">
               <img
-                className={styles["cart-icon"]}
+                className="cart-icon"
                 src="/cart-icon.svg"
                 alt="Account"
               ></img>
             </Link>
           </button>
-          <button className={styles.action}>
+          <button className="action cart">
             <Link>
               <img
-                className={styles["account-icon"]}
+                className="account-icon"
                 src="/account-icon.svg"
                 alt="Cart"
               ></img>
             </Link>
           </button>
+
+          {/* Mobile Menu Icon */}
+          <button className="md:hidden action" onClick={toggleMenu}>
+            <img src="/menu-icon.svg" alt="Menu" className="md:hidden menu-icon"></img>
+          </button>
+        </div>
+        {/* Full-Screen Links */}
+        <div
+          className={`fixed top-0 left-0 w-full h-full bg-white bg-opacity-100 z-50 flex flex-col justify-center items-center text-black space-y-10 text-3xl transition-transform duration-500 ${
+            menuOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <button
+            onClick={toggleMenu}
+            className="absolute top-5 right-5 text-black text-4xl"
+          >
+            ✕
+          </button>
+          <Link to="/catalog" onClick={toggleMenu}>
+            Products
+          </Link>
+          <Link to="/" onClick={toggleMenu}>
+            Pricing
+          </Link>
+          <Link to="/" onClick={toggleMenu}>
+            About
+          </Link>
+          <Link to="/" onClick={toggleMenu}>
+            FAQs
+          </Link>
         </div>
       </div>
     </header>
--- client/src/pages/Catalog/Catalog.jsx
+++ client/src/pages/Catalog/Catalog.jsx
@@ -1,10 +1,12 @@
 // import styles from "./catalog.module.css";
+import Footer from "../../components/Footer/Footer.jsx";
 import Header from "../../components/Header/Header.jsx";
 
 const Catalog = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
