45d2ebe0893d2ceb5d3952c2965c9e445a9c445a
Yermekov Aibar <160598362+F1awless77@users.noreply.github.com>
2025-01-07 15:51:43 +0600

    feat: Finished Catalog design

--- client/src/components/CatalogItems/CatalogItems.jsx
+++ client/src/components/CatalogItems/CatalogItems.jsx
@@ -1,22 +1,122 @@
-import './catalogItems.css'
+import "./catalogItems.css";
+import { Link } from "react-router";
 
 const CatalogItems = () => {
+  return (
+    <section>
+      <div className="flex flex-wrap justify-between mb-6">
 
-    return (
-        <section>
-            <div className="flex flex-wrap justify-between">
-                <div className="catalog-item">
-                    <div>
-                        <img className="sm:w-[49.5vw] lg:w-[19vw]"
-                            src="/nike_dunk_low_retro.png" alt="nike_shoe_image"></img>
-                    </div>
-                    <div>
-                        
-                    </div>
-                </div>
+        <div className="catalog-item mb-6">
+          <Link to="/">
+            <div>
+              <img
+                className="360:w-[49.5vw] lg:w-[19vw]"
+                src="/nike_dunk_low_retro.png"
+                alt="nike_shoe_image"
+              ></img>
             </div>
-        </section>
-    )
-}
+            <div className="mt-2 360:ml-2 lg-ml-0">
+                <h2 className="text-base font-medium">Nike Dunk Low Retro</h2>
+                <p className="text-sm text-gray-500">Shoes</p>
+                <p className="text-sm text-gray-500 mt-1">Men's</p>
+                <h2 className="text-base font-medium mt-1">$130</h2>
+            </div>
+          </Link>
+        </div>
+
+        <div className="catalog-item mb-6">
+          <Link to="/">
+            <div>
+              <img
+                className="360:w-[49.5vw] lg:w-[19vw]"
+                src="/nike_dunk_low_retro.png"
+                alt="nike_shoe_image"
+              ></img>
+            </div>
+            <div className="mt-2 360:ml-2 lg-ml-0">
+                <h2 className="text-base font-medium">Nike Dunk Low Retro</h2>
+                <p className="text-sm text-gray-500">Shoes</p>
+                <p className="text-sm text-gray-500 mt-1">Men's</p>
+                <h2 className="text-base font-medium mt-1">$130</h2>
+            </div>
+          </Link>
+        </div>
+
+        <div className="catalog-item mb-6">
+          <Link to="/">
+            <div>
+              <img
+                className="360:w-[49.5vw] lg:w-[19vw]"
+                src="/nike_dunk_low_retro.png"
+                alt="nike_shoe_image"
+              ></img>
+            </div>
+            <div className="mt-2 360:ml-2 lg-ml-0">
+                <h2 className="text-base font-medium">Nike Dunk Low Retro</h2>
+                <p className="text-sm text-gray-500">Shoes</p>
+                <p className="text-sm text-gray-500 mt-1">Men's</p>
+                <h2 className="text-base font-medium mt-1">$130</h2>
+            </div>
+          </Link>
+        </div>
+
+        <div className="catalog-item mb-6">
+          <Link to="/">
+            <div>
+              <img
+                className="360:w-[49.5vw] lg:w-[19vw]"
+                src="/nike_dunk_low_retro.png"
+                alt="nike_shoe_image"
+              ></img>
+            </div>
+            <div className="mt-2 360:ml-2 lg-ml-0">
+                <h2 className="text-base font-medium">Nike Dunk Low Retro</h2>
+                <p className="text-sm text-gray-500">Shoes</p>
+                <p className="text-sm text-gray-500 mt-1">Men's</p>
+                <h2 className="text-base font-medium mt-1">$130</h2>
+            </div>
+          </Link>
+        </div>
+
+        <div className="catalog-item mb-6">
+          <Link to="/">
+            <div>
+              <img
+                className="360:w-[49.5vw] lg:w-[19vw]"
+                src="/nike_dunk_low_retro.png"
+                alt="nike_shoe_image"
+              ></img>
+            </div>
+            <div className="mt-2 360:ml-2 lg-ml-0">
+                <h2 className="text-base font-medium">Nike Dunk Low Retro</h2>
+                <p className="text-sm text-gray-500">Shoes</p>
+                <p className="text-sm text-gray-500 mt-1">Men's</p>
+                <h2 className="text-base font-medium mt-1">$130</h2>
+            </div>
+          </Link>
+        </div>
+
+        <div className="catalog-item mb-6">
+          <Link to="/">
+            <div>
+              <img
+                className="360:w-[49.5vw] lg:w-[19vw]"
+                src="/nike_dunk_low_retro.png"
+                alt="nike_shoe_image"
+              ></img>
+            </div>
+            <div className="mt-2 360:ml-2 lg-ml-0">
+                <h2 className="text-base font-medium">Nike Dunk Low Retro</h2>
+                <p className="text-sm text-gray-500">Shoes</p>
+                <p className="text-sm text-gray-500 mt-1">Men's</p>
+                <h2 className="text-base font-medium mt-1">$130</h2>
+            </div>
+          </Link>
+        </div>
+        
+      </div>
+    </section>
+  );
+};
 
 export default CatalogItems;
