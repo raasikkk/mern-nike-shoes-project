96c5abefac98f47d9991cc7e52bec2356a28eb87
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2025-01-17 22:11:22 +0500

    without cart test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing.jsx";
 import Catalog from "./pages/Catalog/Catalog.jsx";
-import Cart from "./pages/Cart/Cart.jsx";
+// import Cart from "./pages/Cart/Cart.jsx";
 import Register from "./pages/Register/Register.jsx";
 import Login from "./pages/Login/Login.jsx";
 
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/catalog" element={<Catalog />} />
-          <Route path="/carts/:userId" element={<Cart />} />
+          {/* <Route path="/carts/:userId" element={<Cart />} /> */}
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
